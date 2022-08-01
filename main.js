4bd6502bb6b151d993135eb8999240bc3b4c0b92
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-08-01 17:12:30 +0200

    electron updater #1

--- main.js
+++ main.js
@@ -1,34 +1,51 @@
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
-const {spawn} = require('child_process')
-function createWindow () {
+const {app, BrowserWindow} = require('electron');
+const {autoUpdater} = require('electron-updater');
+const path = require('path');
+const {spawn} = require('child_process');
+
+function createWindow() {
 	const win = new BrowserWindow({
-		width: 800,
-		height: 600,
-		webPreferences: {
-		}
-	})
-
-	win.loadURL('http://localhost:4696')
+		width: 1280,
+		height: 720,
+		webPreferences: {}
+	});
+	win.removeMenu();
+	win.loadURL('http://localhost:4696');
 }
 
-function createServer(){
-	var child = spawn('node', ['server.js'])
+function createServer() {
+	var child = spawn('node', ['server.js']);
 }
+autoUpdater.checkForUpdates()
+autoUpdater.on('update-downloaded', (info) => {
+// Wait 5 seconds, then quit and install
+	// In your application, you don't need to wait for it.
+	// You can call autoUpdater.quitAndInstall();
+	setTimeout(function() {
+		autoUpdater.quitAndInstall();
+	}, 5000);
 
+});
+autoUpdater.on('update-available', (info) => {
+	autoUpdater.downloadUpdate();
+});
+autoUpdater.on('update-not-available', (info) => {
+	alert('No Update available.');
+});
+autoUpdater.on('error', (err) => {
+	alert("Hum, the updater encountered an error. Please try again later.");
+});
 app.whenReady().then(() => {
-	createServer()
-	createWindow()
-
+	createServer();
+	createWindow();
 	app.on('activate', () => {
 		if (BrowserWindow.getAllWindows().length === 0) {
-			createWindow()
+			createWindow();
 		}
-	})
-})
-
+	});
+});
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
-		app.quit()
+		app.quit();
 	}
-})
+});
