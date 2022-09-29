5b0d869fb33ff216c62a0bd883cae597dbf80b88
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2022-09-29 14:51:13 +0200

    bug correction
    ANILIST API not found fix

--- server.js
+++ server.js
@@ -817,7 +817,6 @@ async function API_ANILIST_GET(name) {
 			})
 		};
 	let results = {};
-
 	await fetch(url, options).then(handleResponse).then(handleData).catch(handleError);
 
 	function handleResponse(response) {
@@ -825,16 +824,19 @@ async function API_ANILIST_GET(name) {
 			return response.ok ? json : Promise.reject(json);
 		});
 	}
-		// duplicate an object
+
+	// duplicate an object
 	function clone(obj) {
 		return JSON.parse(JSON.stringify(obj));
 	}
 
-
 	function handleData(data) {
 		console.log(data);
+		if (data.data.Page.media.length === 0) {
+			results = null;
+			return;
+		}
 		let baseObject = clone(data.data.Page.media[0]);
-
 		let staffObject = clone(data.data.Page.media[0].staff.nodes);
 		let charactersObject = clone(data.data.Page.media[0].characters.nodes);
 		let relationsObjectNodes = clone(data.data.Page.media[0].relations.nodes);
@@ -845,9 +847,7 @@ async function API_ANILIST_GET(name) {
 			relationsObject[i]["relationType"] = relationsObjectEdges[i].relationType;
 		}
 		delete baseObject["relations"];
-
-
-		for (let i=0;i<baseObject.staff.nodes.length;i++) {
+		for (let i = 0; i < baseObject.staff.nodes.length; i++) {
 			for (let key in baseObject.staff.nodes[i]) {
 				if (key !== "id" && key !== "name") {
 					delete baseObject.staff.nodes[i][key];
@@ -856,8 +856,7 @@ async function API_ANILIST_GET(name) {
 			baseObject.staff.nodes[i]["name"] = baseObject.staff.nodes[i]["name"]["full"];
 		}
 		baseObject.staff = baseObject.staff.nodes;
-
-		for (let i=0;i<baseObject.characters.nodes.length;i++) {
+		for (let i = 0; i < baseObject.characters.nodes.length; i++) {
 			for (let key in baseObject.characters.nodes[i]) {
 				if (key !== "id" && key !== "name") {
 					delete baseObject.characters.nodes[i][key];
@@ -873,7 +872,9 @@ async function API_ANILIST_GET(name) {
 			"relations": relationsObject
 		}
 	}
+
 	return results;
+
 	function handleError(error) {
 		console.error(error);
 	}
@@ -885,49 +886,49 @@ app.post("/api/anilist", async (req, res) => {
 	let path = req.headers.path;
 	await API_ANILIST_GET(name).then(async function (thedata) {
 		console.log(thedata);
+		let randID = Math.floor(Math.random() * 1000000);
+		if (thedata === null) {
+			await insertIntoDB("(ID_Series,title,note,statut,start_date,end_date,description,Score,genres,cover,BG,CHARACTERS,TRENDING,STAFF,SOURCE,volumes,chapters,favorite,PATH,lock)", "('" + randID + "U_2" + "','" + JSON.stringify(name.replaceAll("'", "''")) + "',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,0,'" + path + "',false)", token, "Series");
+			return;
+		}
 		let data = thedata["base"];
 		let relationsData = thedata["relations"];
 		let charactersData = thedata["characters"];
 		let staffData = thedata["staff"];
 		console.log(staffData);
-		let randID = Math.floor(Math.random() * 1000000);
-		if (data === null) {
-			await insertIntoDB("(ID_Series,title,note,statut,start_date,end_date,description,Score,genres,cover,BG,CHARACTERS,TRENDING,STAFF,SOURCE,volumes,chapters,favorite,PATH,lock)", "('" + randID + "U_2" + "','" + JSON.stringify(name.replaceAll("'", "''")) + "',null,null,null,null,null,'0',null,null,null,null,null,null,null,null,null,0,'" + path + "',false)", token, "Series");
-		} else {
-			await insertIntoDB("(ID_Series,title,note,statut,start_date,end_date,description,Score,genres,cover,BG,CHARACTERS,TRENDING,STAFF,SOURCE,volumes,chapters,favorite,PATH,lock)", "('" + data["id"] + "_2" + "','" + JSON.stringify(data["title"]).replaceAll("'", "''") + "',null,'" + data["status"].replaceAll("'", "''") + "','" + JSON.stringify(data["startDate"]).replaceAll("'", "''") + "','" + JSON.stringify(data["endDate"]).replaceAll("'", "''") + "','" + data["description"].replaceAll("'", "''") + "','" + data["meanScore"] + "','" + JSON.stringify(data["genres"]).replaceAll("'", "''") + "','" + data["coverImage"]["large"] + "','" + data["bannerImage"] + "','" + JSON.stringify(data["characters"]).replaceAll("'", "''") + "','" + data["trending"] + "','" + JSON.stringify(data["staff"]).replaceAll("'", "''") + "','" + data["siteUrl"].replaceAll("'", "''") + "','" + data["volumes"] + "','" + data["chapters"] + "',0,'" + path + "',false)", token, "Series");
-		}
-			for (let i = 0; i < staffData.length; i++) {
-				try {
-					if (staffData[i]["description"] == null) {
-						await insertIntoDB("", `('${staffData[i]["id"] + "_2"}','${staffData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(staffData[i]["image"]["medium"])}','${null}','${JSON.stringify(staffData[i]["siteUrl"])}')`, token, "Creators")
-					} else {
-						await insertIntoDB("", `('${staffData[i]["id"] + "_2"}','${staffData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(staffData[i]["image"]["medium"])}','${JSON.stringify(staffData[i]["description"].replaceAll("'", "''"))}','${JSON.stringify(staffData[i]["siteUrl"])}')`, token, "Creators")
-					}
-				} catch (e) {
-					console.log(e);
-				}
-			}
-			for (let i = 0; i < charactersData.length; i++) {
-				try {
-					if (charactersData[i]["description"] == null) {
-						await insertIntoDB("", `('${charactersData[i]["id"] + "_2"}','${charactersData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(charactersData[i]["image"]["medium"])}','${null}','${JSON.stringify(charactersData[i]["siteUrl"])}')`, token, "Characters")
-					} else {
-						await insertIntoDB("", `('${charactersData[i]["id"] + "_2"}','${charactersData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(charactersData[i]["image"]["medium"])}','${JSON.stringify(charactersData[i]["description"].replaceAll("'", "''"))}','${JSON.stringify(charactersData[i]["siteUrl"])}')`, token, "Characters")
-					}
-				} catch (e) {
-					console.log(e);
-				}
-			}
-			for (let i = 0; i < relationsData.length; i++) {
-				let dataR = relationsData[i];
-				if (dataR.title.english == null) {
-					await insertIntoDB("", `('${dataR["id"] + "_2"}','${dataR["title"]["romaji"].replaceAll("'", "''")}','${dataR["coverImage"]["large"]}','${dataR["type"] + " / " + dataR["relationType"] + " / " + dataR["format"]}',${null},'${data["id"] + "_2"}')`, token, "relations");
-					console.log("inserted");
+		await insertIntoDB("(ID_Series,title,note,statut,start_date,end_date,description,Score,genres,cover,BG,CHARACTERS,TRENDING,STAFF,SOURCE,volumes,chapters,favorite,PATH,lock)", "('" + data["id"] + "_2" + "','" + JSON.stringify(data["title"]).replaceAll("'", "''") + "',null,'" + data["status"].replaceAll("'", "''") + "','" + JSON.stringify(data["startDate"]).replaceAll("'", "''") + "','" + JSON.stringify(data["endDate"]).replaceAll("'", "''") + "','" + data["description"].replaceAll("'", "''") + "','" + data["meanScore"] + "','" + JSON.stringify(data["genres"]).replaceAll("'", "''") + "','" + data["coverImage"]["large"] + "','" + data["bannerImage"] + "','" + JSON.stringify(data["characters"]).replaceAll("'", "''") + "','" + data["trending"] + "','" + JSON.stringify(data["staff"]).replaceAll("'", "''") + "','" + data["siteUrl"].replaceAll("'", "''") + "','" + data["volumes"] + "','" + data["chapters"] + "',0,'" + path + "',false)", token, "Series");
+		for (let i = 0; i < staffData.length; i++) {
+			try {
+				if (staffData[i]["description"] == null) {
+					await insertIntoDB("", `('${staffData[i]["id"] + "_2"}','${staffData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(staffData[i]["image"]["medium"])}','${null}','${JSON.stringify(staffData[i]["siteUrl"])}')`, token, "Creators")
 				} else {
-					await insertIntoDB("", `('${dataR["id"] + "_2"}','${dataR["title"]["english"].replaceAll("'", "''")}','${dataR["coverImage"]["large"]}','${dataR["type"] + " / " + dataR["relationType"] + " / " + dataR["format"]}',${null},'${data["id"] + "_2"}')`, token, "relations");
-					console.log("inserted");
+					await insertIntoDB("", `('${staffData[i]["id"] + "_2"}','${staffData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(staffData[i]["image"]["medium"])}','${JSON.stringify(staffData[i]["description"].replaceAll("'", "''"))}','${JSON.stringify(staffData[i]["siteUrl"])}')`, token, "Creators")
 				}
+			} catch (e) {
+				console.log(e);
 			}
+		}
+		for (let i = 0; i < charactersData.length; i++) {
+			try {
+				if (charactersData[i]["description"] == null) {
+					await insertIntoDB("", `('${charactersData[i]["id"] + "_2"}','${charactersData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(charactersData[i]["image"]["medium"])}','${null}','${JSON.stringify(charactersData[i]["siteUrl"])}')`, token, "Characters")
+				} else {
+					await insertIntoDB("", `('${charactersData[i]["id"] + "_2"}','${charactersData[i]["name"]["full"].replaceAll("'", "''")}','${JSON.stringify(charactersData[i]["image"]["medium"])}','${JSON.stringify(charactersData[i]["description"].replaceAll("'", "''"))}','${JSON.stringify(charactersData[i]["siteUrl"])}')`, token, "Characters")
+				}
+			} catch (e) {
+				console.log(e);
+			}
+		}
+		for (let i = 0; i < relationsData.length; i++) {
+			let dataR = relationsData[i];
+			if (dataR.title.english == null) {
+				await insertIntoDB("", `('${dataR["id"] + "_2"}','${dataR["title"]["romaji"].replaceAll("'", "''")}','${dataR["coverImage"]["large"]}','${dataR["type"] + " / " + dataR["relationType"] + " / " + dataR["format"]}',${null},'${data["id"] + "_2"}')`, token, "relations");
+				console.log("inserted");
+			} else {
+				await insertIntoDB("", `('${dataR["id"] + "_2"}','${dataR["title"]["english"].replaceAll("'", "''")}','${dataR["coverImage"]["large"]}','${dataR["type"] + " / " + dataR["relationType"] + " / " + dataR["format"]}',${null},'${data["id"] + "_2"}')`, token, "relations");
+				console.log("inserted");
+			}
+		}
 	})
 	res.sendStatus(200)
 });
