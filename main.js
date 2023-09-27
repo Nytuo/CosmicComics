55cb030acd6bd4668e197c353f261287a5375448
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2023-09-27 16:40:22 +0200

    refactor: :fire: Deleting files and part of code

--- main.js
+++ main.js
@@ -36,57 +36,6 @@ function createServer() {
 }
 
 autoUpdater.checkForUpdatesAndNotify();
-let url = "https://github.com/Nytuo/CosmicComics";
-let branch = "master";
-let gitOptions = {
-	repo: url,
-	branch: branch,
-	baseDir: __dirname,
-
-}
-let git = require("simple-git")(gitOptions);
-var isWin = require('os').platform().indexOf('win') > -1;
-
-var where = isWin ? 'where' : 'type';
-
-var spawn = require('child_process').spawn;
-
-var out = spawn(where + ' git', [], { shell: true });
-
-function PrepareLaunch() {
-	fs.access(__dirname, fs.constants.W_OK, (err) => {
-		if (err) {
-			console.log("Can't write on the directory, skipping update");
-			launch();
-		} else {
-			if (fs.existsSync(path.join(__dirname, '.git'))) {
-				updateAndLaunch();
-			} else {
-				console.log("No git repository found, initializing...");
-				git.init(false, {}, function (err, init) {
-					console.log(err);
-					git.addRemote('origin', url + '.git', function (err) {
-						console.log(err);
-						updateAndLaunch();
-					});
-				});
-			}
-		}
-	});
-}
-
-out.on('close', function (code) {
-	console.log('git exit code : ' + code);
-	if (code === 0) {
-		//PrepareLaunch();
-		launch();
-	} else {
-		console.log("Git not installed, skipping update");
-		launch();
-	}
-});
-
-
 
 function launch() {
 	createServer();
@@ -99,31 +48,9 @@ function launch() {
 }
 
 app.whenReady().then(() => {
-
-
-
+	launch();
 });
 
-function updateAndLaunch() {
-	git.fetch("origin", branch, [], function (err, fetchResult) {
-		console.log(err);
-		git.reset(['--hard'], function (err, update) {
-			console.log(err);
-			git.pull("origin", branch, ["--force"], function (err, pull) {
-				console.log(err);
-				git.checkout(['origin/' + branch, '--force'], function (err, update) {
-					console.log(err);
-					if (err) {
-						app.quit();
-					} else {
-						launch();
-					}
-
-				});
-			})
-		});
-	});
-}
 app.on('window-all-closed', () => {
 	if (process.platform !== 'darwin') {
 		app.quit();
