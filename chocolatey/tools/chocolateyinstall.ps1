e0dfd71e336326f97cdd75ecb372ae7077ac0712
Arnaud BEUX <34894524+Nytuo@users.noreply.github.com>
2025-01-02 19:41:22 +0100

    chore: chocolatey pushed to 2.1.1

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop' # stop on all errors
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.nytuo.fr/CosmicComics/v2.1.0/Cosmic-Comics_2.1.0_win.exe' # download url, HTTPS preferred
+$url        = 'https://download.nytuo.fr/CosmicComics/v2.1.1/Cosmic-Comics_2.1.1_win.exe' # download url, HTTPS preferred
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -8,7 +8,7 @@ $packageArgs = @{
   fileType      = 'EXE' #only one of these: exe, msi, msu
   url           = $url
   softwareName  = 'cosmic-comics*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
-  checksum      = '405618bc8c68b409202f84f75b7658786c2b5b1b3910006a79bebbe5b319cc0ca6384eb7ae6c1229e34ca0f3703a14a655b0b9312da6da598fbc5d25bb918792'
+  checksum      = 'd625550d7d584657caaa6ea3b0808948d9a9cac7cbc064c2758b740e0a96e57f7d401c93de30cef2d2707fd08e10f225bf14b87430c949f9cc9b28228894b001'
   checksumType  = 'sha512' #default is md5, can also be sha1, sha256 or sha512
   silentArgs   = '/S'           # NSIS
   validExitCodes= @(0,1,2) #please insert other valid exit codes here
